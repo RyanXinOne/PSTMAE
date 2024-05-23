4a38f3e58e880311edd7155fd890f822f58688c7
Ryan Xin <xyz@ryanxin.cn>
2024-05-23 15:33:02 +0100

    Update plot

--- plot/plot_mse.py
+++ plot/plot_mse.py
@@ -26,23 +26,78 @@ for model in ['timae', 'convrae', 'convlstm']:
         stats[stat]['values'] = np.array(stats[stat]['values'])
     all_stats[model] = stats
 
-fig, axs = plt.subplots(1, 3, figsize=(12, 3), gridspec_kw={'width_ratios': [2, 2, 1]})
-for i, stat in enumerate(['train/mse', 'val/mse', 'test/mse']):
-    for j, model in enumerate(all_stats.keys()):
-        # Extract mean and std
-        steps = all_stats[model][stat]['steps'][0]
-        mean_values = np.mean(all_stats[model][stat]['values'], axis=0)
-        std_values = np.std(all_stats[model][stat]['values'], axis=0)
+event_acc = event_accumulator.EventAccumulator(f"logs/autoencoder/lightning_logs/prod")
+event_acc.Reload()
+ae_mse = event_acc.Scalars('test/mse')[0].value
 
-        # Visualize the data
-        if i < 2:
-            axs[i].plot(steps, mean_values, label=model)
-            axs[i].fill_between(steps, mean_values - std_values, mean_values + std_values, alpha=0.3)
-        else:
-            axs[i].errorbar(j, mean_values, yerr=std_values, fmt='o', capsize=5, label=model)
-    axs[i].set_xlabel('Step')
-    axs[i].set_ylabel('Value')
-    axs[i].legend()
-    axs[i].set_title(stat)
+
+fig, axs = plt.subplots(1, 3, figsize=(12, 3), gridspec_kw={'width_ratios': [2, 2, 1]})
+
+i, stat = 0, 'train/mse'
+max_steps = max([all_stats[model][stat]['steps'][0][-1] for model in all_stats.keys()])
+for j, model in enumerate(all_stats.keys()):
+    # Extract mean and std
+    steps = all_stats[model][stat]['steps'][0]
+    mean_values = np.mean(all_stats[model][stat]['values'], axis=0)
+    std_values = np.std(all_stats[model][stat]['values'], axis=0)
+
+    # Visualize the data
+    axs[i].plot(steps, mean_values, label=model)
+    axs[i].fill_between(steps, mean_values - std_values, mean_values + std_values, alpha=0.3)
+
+    # Extend the ending values if necessary
+    if steps[-1] < max_steps:
+        extra_steps = np.arange(steps[-1], max_steps+1)
+        extended_mean_values = np.full(len(extra_steps), mean_values[-1])
+
+        # Plot the extended part with dotted lines
+        axs[i].plot(extra_steps, extended_mean_values, linestyle='--', color=axs[i].lines[-1].get_color())
+axs[i].axhline(ae_mse, linestyle='--', color='r', alpha=0.5,  label='autoencoder')
+axs[i].set_yscale('log')
+axs[i].set_xlabel('Step')
+axs[i].set_ylabel('Value (log scale)')
+axs[i].legend()
+axs[i].set_title(stat)
+
+i, stat = 1, 'val/mse'
+max_steps = max([all_stats[model][stat]['steps'][0][-1] for model in all_stats.keys()])
+for j, model in enumerate(all_stats.keys()):
+    # Extract mean and std
+    steps = all_stats[model][stat]['steps'][0]
+    mean_values = np.mean(all_stats[model][stat]['values'], axis=0)
+    std_values = np.std(all_stats[model][stat]['values'], axis=0)
+
+    # Visualize the data
+    axs[i].plot(steps, mean_values, label=model)
+    axs[i].fill_between(steps, mean_values - std_values, mean_values + std_values, alpha=0.3)
+
+    # Extend the ending values if necessary
+    if steps[-1] < max_steps:
+        extra_steps = np.arange(steps[-1], max_steps+1)
+        extended_mean_values = np.full(len(extra_steps), mean_values[-1])
+
+        # Plot the extended part with dotted lines
+        axs[i].plot(extra_steps, extended_mean_values, linestyle='--', color=axs[i].lines[-1].get_color())
+axs[i].axhline(ae_mse, linestyle='--', color='r', alpha=0.5,  label='autoencoder')
+axs[i].set_xlabel('Step')
+axs[i].set_ylabel('Value')
+axs[i].legend()
+axs[i].set_title(stat)
+
+i, stat = 2, 'test/mse'
+for j, model in enumerate(all_stats.keys()):
+    # Extract mean and std
+    steps = all_stats[model][stat]['steps'][0]
+    mean_values = np.mean(all_stats[model][stat]['values'], axis=0)
+    std_values = np.std(all_stats[model][stat]['values'], axis=0)
+
+    # Visualize the data
+    axs[i].errorbar(j, mean_values, yerr=std_values, fmt='o', capsize=5, label=model)
+axs[i].axhline(ae_mse, linestyle='--', color='r', alpha=0.5,  label='autoencoder')
+axs[i].set_xticks([])
+axs[i].set_ylabel('Value')
+axs[i].legend()
+axs[i].set_title(stat)
+
 plt.tight_layout()
 plt.show()
